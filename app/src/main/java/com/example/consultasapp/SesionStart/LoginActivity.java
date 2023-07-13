88e905fddc83d5885c5835ac5e23b3aeabb1e722
rey-Adriano <2017110727@udh.edu.pe>
2023-07-13 15:17:21 -0500

    update login

--- app/src/main/java/com/example/consultasapp/SesionStart/LoginActivity.java
+++ app/src/main/java/com/example/consultasapp/SesionStart/LoginActivity.java
@@ -1,37 +1,76 @@
 package com.example.consultasapp.SesionStart;
 
-import static android.content.ContentValues.TAG;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.consultasapp.R;
-import com.example.consultasapp.databinding.ActivityLoginBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.HashMap;
-import java.util.Map;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
-
+    Button Bingresar;
+    TextView Lpassword, Lemail;
+    FirebaseAuth UAuth;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        com.example.consultasapp.databinding.ActivityLoginBinding binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_login);
-        setContentView(binding.getRoot());
+        Bingresar = findViewById(R.id.BtnIngresar);
+        Lpassword = findViewById(R.id.InputPassword1);
+        Lemail= findViewById(R.id.InputEmial1);
+        UAuth = FirebaseAuth.getInstance();
 
+        Bingresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String emailUser = Lemail.getText().toString().trim();
+                String passUser = Lpassword.getText().toString().trim();
+
+
+                if (emailUser.isEmpty() && passUser.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Ingresar datos", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    loginUser(emailUser, passUser);
+
+                }
+            }
+        });
+
+    }
+
+    private void loginUser(String emailUser, String passUser) {
+        UAuth.signInWithEmailAndPassword(emailUser, passUser).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    finish();
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    Toast.makeText(LoginActivity.this, "Bienvenido", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(LoginActivity.this, "Error Usuario Invalido", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(LoginActivity.this, "error en el inicio de sesión", Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 
     public void IntoNewUser(View view) {
@@ -40,37 +79,8 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
-
     public void ingresar(View view) {
-        Button miBoton = findViewById(R.id.ingresar);
-        miBoton.setOnClickListener(v -> addDataFirestore()
-            );
 
     }
-
-    private void addDataFirestore(){
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        Map<String, Object> user = new HashMap<>();
-        user.put("nombre", "mario");
-        user.put("apellido",  "MARIA");
-        user.put("edad", 23);
-        db.collection("usuarios")
-                .add(user)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d(TAG, "added ID: "+documentReference.getId());
-
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "error add documents", e);
-                    }
-                });
-
-    }
-
 
 }
